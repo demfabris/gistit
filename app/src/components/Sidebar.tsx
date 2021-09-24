aa3b5f04f39be1906cca360f2174846d61cbccdc
Fabricio Dematte <fabriciodematte7p@gmail.com>
2021-09-24 18:37:03 -0300

    feat: [UI] add sidebar links and sublinks

--- app/src/components/Sidebar.tsx
+++ app/src/components/Sidebar.tsx
@@ -1,5 +1,14 @@
-import { Dispatch, SetStateAction } from "react";
-import { VscChromeClose } from "react-icons/vsc";
+import { Dispatch, SetStateAction, useState } from "react";
+import {
+  VscChromeClose,
+  VscColorMode,
+  VscDesktopDownload,
+  VscGithub,
+  VscLibrary,
+  VscLightbulb,
+  VscSearch,
+  VscTerminal,
+} from "react-icons/vsc";
 
 interface Props {
   sidebarHandler: [boolean, Dispatch<SetStateAction<boolean>>];
@@ -7,23 +16,105 @@ interface Props {
 export const Sidebar = ({ sidebarHandler }: Props) => {
   const [state, setState] = sidebarHandler;
 
-  function handleOpen(state: boolean): String {
-    return state ? "translate-x-full" : "";
-  }
-
   return (
-    <div
-      className={`fixed flex justify-end right-0 top-0 w-full h-full z-10 transition 
-        transform-gpu ${handleOpen(state)}`}
-    >
-      <div className="p-6 w-4/6 bg-white h-full shadow-2xl">
+    <>
+      <div
+        className={`fixed flex right-0 top-0 w-full h-full z-10 bg-black transition-all ${
+          state ? "opacity-30" : "pointer-events-none opacity-0"
+        }`}
+      />
+      <aside
+        className={`fixed right-0 top-0 w-4/6 bg-white h-full shadow-2xl z-20 transition transform ${
+          state ? "" : "translate-x-full"
+        }`}
+      >
         <button
           className="absolute right-6 top-9"
           onClick={() => setState((state) => !state)}
         >
           <VscChromeClose size={24} />
         </button>
-      </div>
+        <nav className="pt-24 w-full">
+          <Link text="Search" href="/search" icon={<VscSearch size={20} />} />
+          <LinkDropDown text="Documentation" icon={<VscLibrary size={20} />} />
+          <Link
+            text="Github"
+            href="https://github.com/fabricio7p"
+            icon={<VscGithub size={20} />}
+          />
+          <Link
+            text="Mode"
+            href="https://github.com/fabricio7p"
+            icon={<VscColorMode size={20} />}
+          />
+        </nav>
+      </aside>
+    </>
+  );
+};
+
+interface LinkProps {
+  text: String;
+  icon: React.ReactElement;
+  href: string;
+}
+const Link = ({ text, icon, href }: LinkProps) => {
+  return (
+    <div>
+      <a
+        href={href}
+        className="h-16 cursor-pointer pl-8 text-sm font-semibold text-gray-700 flex items-center border-l-4 border-transparent
+        hover:border-blue-500"
+      >
+        <i className="pr-4">{icon}</i>
+        {text}
+      </a>
     </div>
+  );
+};
+
+interface LinkDropDownProps {
+  text: string;
+  icon: React.ReactElement;
+}
+const LinkDropDown = ({ text, icon }: LinkDropDownProps) => {
+  const [drop, setDrop] = useState(false);
+
+  return (
+    <div className="flex flex-col">
+      <div
+        onClick={() => setDrop((state) => !state)}
+        className={`cursor-pointer pl-8 text-sm h-16 flex items-center font-semibold text-gray-700 border-l-4 border-transparent
+          hover:border-blue-500 ${drop ? "border-blue-500" : ""}`}
+      >
+        <i className="pr-4">{icon}</i>
+        {text}
+      </div>
+      <ul className={`${drop ? "flex flex-col h-full" : "hidden"}`}>
+        <SubLink
+          text="Installation"
+          href="#"
+          icon={<VscDesktopDownload size={18} />}
+        />
+        <SubLink text="Features" href="#" icon={<VscLightbulb size={18} />} />
+        <SubLink text="CLI" href="#" icon={<VscTerminal size={18} />} />
+      </ul>
+    </div>
+  );
+};
+
+interface SubLinkProps {
+  text: string;
+  href: string;
+  icon?: React.ReactElement;
+}
+const SubLink = ({ text, href, icon }: SubLinkProps) => {
+  return (
+    <li className="cursor-pointer flex font-semibold items-center pl-16 text-gray-700 w-full h-12 text-xs border-blue-200 border-l-4 hover:border-blue-500">
+      <a href={href} className="flex items-center">
+        <i className="pr-4">{icon}</i>
+        {text}
+      </a>
+    </li>
   );
 };
--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,19 +1,54 @@
 import type { NextPage } from "next";
+import { Layout } from "components";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
-    <section>
-      <header>
-        <div className="pl-16 pr-16 h-16 w-full flex border-b-2 bg-gray-100 items-center">
-          <h1 className="font-bold text-xl pl-10 pr-10 h-full bg-blue-500 flex items-center justify-center">
-            Gistit
-          </h1>
-          <ul className="flex justify-center items-center">
-            <li className="">link</li>
-          </ul>
-        </div>
-      </header>
-    </section>
+    <Layout withHeaderBar>
+      <>
+        {/* <Image src="/logo.svg" alt="logo" width="300px" height="150px" /> */}
+        <p className="text-7xl font-black text-gray-800">
+          Gistit<b className="text-blue-500">.</b>
+        </p>
+        <span className="text-xl font-thin text-center mt-4 mb-12 text-gray-700">
+          Quick and easy <b className="font-bold">anonymous</b> code snippet
+          sharing.
+        </span>
+        <button className="rounded-full bg-blue-500 text-white text-lg font-bold h-14 px-8 shadow-sm">
+          Get started
+        </button>
+        <div className="w-full flex border-b-2 mt-20"></div>
+        <ul className="w-full flex flex-col px-6 md:flex-row md:px-0 justify-between mt-12 gap-x-16 gap-y-10">
+          <li className="flex flex-col">
+            <h2 className="font-semibold text-xl">Practical</h2>
+            <span className="text-md font-thin">
+              Easy to use cli tool to quickly upload any code snippet to a
+              temporary web page.
+            </span>
+          </li>
+          <li className="flex flex-col">
+            <h2 className="font-semibold text-xl">Practical</h2>
+            <span className="text-md font-thin">
+              Easy to use cli tool to quickly upload any code snippet to a
+              temporary web page.
+            </span>
+          </li>
+          <li className="flex flex-col">
+            <h2 className="font-semibold text-xl">Practical</h2>
+            <span className="text-md font-thin">
+              Easy to use cli tool to quickly upload any code snippet to a
+              temporary web page.
+            </span>
+          </li>
+        </ul>
+        <video
+          src="#"
+          width="500px"
+          height="400px"
+          className="mt-20 bg-gray-300"
+        />
+      </>
+    </Layout>
   );
 };
 
