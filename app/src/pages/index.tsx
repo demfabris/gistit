235a6850eb6f6136f752f376e303d1675c6d2cfc
Fabricio Dematte <fabriciodematte7p@gmail.com>
2021-09-25 23:18:52 -0300

    feat: [UI] add minimum docs to index page

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,9 +1,8 @@
 import type { NextPage } from 'next'
-import { Layout } from 'components'
-import Code from 'react-syntax-highlighter'
+import { Heading, Layout, SubHeading, Code } from 'components'
+import { DocLink, DocText } from 'components/Docs'
 
 const Home: NextPage = () => {
-  const codeString = 'const aux = "hello world"'
   return (
     <Layout withHeaderBar>
       <>
@@ -18,26 +17,26 @@ const Home: NextPage = () => {
           Get started
         </button>
         <div className="flex w-full mt-20 border-b-2 border-gray-200 dark:border-gray-700"></div>
-        <ul className="flex flex-col justify-between w-full px-6 mt-12 md:flex-row md:px-0 gap-x-16 gap-y-10">
-          <li className="flex flex-col">
-            <h2 className="text-xl font-semibold">Practical</h2>
-            <span className="font-thin text-md">
-              Easy to use cli tool to quickly upload any code snippet to a
-              temporary web page.
+        <ul className="flex flex-col justify-between w-full px-6 mt-16 md:flex-row md:px-0 gap-x-14 gap-y-10">
+          <li className="flex flex-col w-full">
+            <h2 className="mb-3 text-xl font-semibold">Practical</h2>
+            <span className="font-thin leading-7">
+              Easy to use <b>cli</b> tool to quickly share a code snippet or a
+              couple of files, with syntax highlighting!
             </span>
           </li>
-          <li className="flex flex-col">
-            <h2 className="text-xl font-semibold">Practical</h2>
-            <span className="font-thin text-md">
-              Easy to use cli tool to quickly upload any code snippet to a
-              temporary web page.
+          <li className="flex flex-col w-full">
+            <h2 className="mb-3 text-xl font-semibold">Terminal Support</h2>
+            <span className="font-thin leading-7">
+              Shared snippets can be accessed via web page or by entering a
+              unique <b>hash</b> directly in your terminal.
             </span>
           </li>
-          <li className="flex flex-col">
-            <h2 className="text-xl font-semibold">Practical</h2>
-            <span className="font-thin text-md">
-              Easy to use cli tool to quickly upload any code snippet to a
-              temporary web page.
+          <li className="flex flex-col w-full">
+            <h2 className="mb-3 text-xl font-semibold">Rust-Powered</h2>
+            <span className="font-thin leading-7">
+              Fast, safe, and lightweight <b>cli</b> application to just run,
+              share and move on with your life.
             </span>
           </li>
         </ul>
@@ -47,9 +46,37 @@ const Home: NextPage = () => {
           height="481px"
           className="my-20 bg-gray-200 dark:bg-gray-700"
         />
-        <Code language="javascript" className="rounded-lg">
-          {codeString}
-        </Code>
+        <article className="flex flex-col w-full text-left">
+          <Heading
+            text="Prerequisites"
+            id="prerequisites"
+            href="#prerequisites"
+          />
+          <SubHeading text="Clipboard" />
+          <div className="pl-6">
+            <DocText text="For clipboard features make sure you have a working clipboard backend." />
+          </div>
+          <SubHeading text="Terminal colors" />
+          <div className="pl-6">
+            <DocText text="To get syntax highlighting when fetching snippets from the terminal make sure you have 256-bit colored terminal. " />
+            <DocLink text="Term colors" href="#" />
+          </div>
+          <Heading text="Installation" id="installation" href="#installation" />
+          Gistit is avaiable in most distribution channels.
+          <SubHeading text="Quick Install" />
+          <div className="pl-6">
+            <DocText text="With shell : " />
+            <Code
+              codeString="sh -c '$(curl -fsSL https://gistit.io/install.sh)'"
+              lang="sh"
+            />
+          </div>
+          <SubHeading text="Cli binary" />
+          <div className="pl-6">
+            <DocText text="Get the latest build for your system here : " />
+            <DocLink text="Github" href="#" />
+          </div>
+        </article>
       </>
     </Layout>
   )
