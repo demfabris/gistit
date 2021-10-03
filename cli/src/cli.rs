b295db6d90a7c39fed63c85f2349d7a559c5097d
Fabricio Dematte <fabriciodematte7p@gmail.com>
2021-10-03 17:20:16 -0300

    refactor: [CLI] make cli fields public

--- cli/src/cli.rs
+++ cli/src/cli.rs
@@ -1,51 +1,57 @@
 //! Gistit command line interface
 //!
+use std::ffi::OsString;
+
 /// Share or get a gistit.
 #[derive(argh::FromArgs, PartialEq, Debug)]
 pub struct MainArgs {
     /// dry run mode. check for platform requirements
     #[argh(switch, short = 'r')]
-    dry_run: bool,
+    pub dry_run: bool,
     /// list avaiable colorschemes
     #[argh(switch, short = 't')]
-    colorschemes: bool,
+    pub colorschemes: bool,
     /// action
     #[argh(subcommand)]
-    action: SubCommands,
+    pub action: SubCommands,
 }
 /// Subcommands variations
+#[non_exhaustive]
 #[derive(argh::FromArgs, PartialEq, Debug)]
 #[argh(subcommand)]
-enum SubCommands {
+pub enum SubCommands {
     Send(SendArgs),
     Fetch(FetchArgs),
 }
 /// Upload a gistit to the cloud
 #[derive(argh::FromArgs, PartialEq, Debug)]
 #[argh(subcommand, name = "send")]
-struct SendArgs {
+pub struct SendArgs {
+    /// the file. currently one supported
+    #[argh(option, short = 'f')]
+    pub file: OsString,
     /// with a description
     #[argh(option, short = 'd')]
-    description: Option<String>,
+    pub description: Option<String>,
     /// with author details
     #[argh(option, short = 'a')]
-    author: Option<String>,
+    pub author: Option<String>,
     /// with password encryption
     #[argh(option, short = 's')]
-    secret: Option<String>,
+    pub secret: Option<String>,
     /// blank
     #[argh(
         option,
         short = 't',
         description = "choose a colorscheme. 'gistit -t' for available colorschemes"
     )]
-    theme: Option<String>,
+    pub theme: Option<String>,
     /// store output hash in clipboard. (on successful upload)
     #[argh(switch, short = 'c')]
-    clipboard: bool,
+    pub clipboard: bool,
     /// custom lifetime in seconds. DEFAULT = 3600, MAX = 3600
     #[argh(option, short = 'l')]
-    lifetime: Option<i16>,
+    pub lifetime: Option<u16>,
 }
 /// Fetch a gistit
 #[derive(argh::FromArgs, PartialEq, Debug)]
@@ -54,14 +60,14 @@ struct SendArgs {
     name = "fetch",
     note = "The default filesystem save location is based on you platform..."
 )]
-struct FetchArgs {
+pub struct FetchArgs {
     /// provide the secret to decrypt (if any)
     #[argh(option, short = 's')]
-    secret: Option<String>,
+    pub secret: Option<String>,
     /// no syntax highlighting
     #[argh(switch, short = 'o')]
-    no_syntax_highlighting: bool,
+    pub no_syntax_highlighting: bool,
     /// save a copy on local filesystem
     #[argh(switch, short = 'v')]
-    save: bool,
+    pub save: bool,
 }
