3b677b91eea4d31091bc9d31631e7d8a9d642430
Fabricio Dematte <fabriciodematte7p@gmail.com>
2022-01-30 13:39:53 -0300

    fix: body min height

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="flex flex-row items-center justify-center w-full h-24 mt-20 text-xs font-medium text-center text-gray-500 border-t-2 border-gray-200 dark:border-gray-700">
+    <footer className="flex flex-row items-center justify-center w-5/6 h-24 mt-auto text-xs font-medium text-center text-gray-500 border-t-2 border-gray-200 dark:border-gray-700">
       MIT Licensed | Copyright © 2021-present Gistit Contributors
     </footer>
   )
---
+++ web/src/pages/auth.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import { Heading, Layout, SubHeading, Code } from 'components'
+
+const Auth: NextPage = () => {
+  return (
+    <Layout withHeaderBar>
+      <div className="">hello</div>
+    </Layout>
+  )
+}
+
+export default Auth
