a0bcc92fe47eefe20fcf6d8af9db9128ae10fecc
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-02-03 20:56:56 -0500

    :sparkles: Add Title

--- web/src/components/Layout.tsx
+++ web/src/components/Layout.tsx
@@ -1,21 +1,34 @@
 import { useState } from 'react'
+import Head from 'next/head'
 import { Header, Sidebar, Footer } from 'components'
 
+import type { FC } from 'react'
+
 interface Props {
-  children: React.ReactChild
   withHeaderBar: boolean
+  title: string
 }
-export const Layout = ({ children, withHeaderBar }: Props) => {
+
+export const Layout: FC<Props> = ({
+  children,
+  withHeaderBar,
+  title = 'Gistit'
+}) => {
   const sidebarHandler = useState(false)
 
   return (
-    <section className="flex flex-col items-center justify-center min-h-screen">
-      <Header withHeaderBar={withHeaderBar} sidebarHandler={sidebarHandler} />
-      <div className="flex flex-col items-center w-full px-6 md:px-0 md:w-layout">
-        <Sidebar sidebarHandler={sidebarHandler} />
-        {children}
-      </div>
-      <Footer />
-    </section>
+    <div>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <section className="flex flex-col items-center justify-center min-h-screen">
+        <Header withHeaderBar={withHeaderBar} sidebarHandler={sidebarHandler} />
+        <div className="flex flex-col items-center w-full px-6 md:px-0 md:w-layout">
+          <Sidebar sidebarHandler={sidebarHandler} />
+          {children}
+        </div>
+        <Footer />
+      </section>
+    </div>
   )
 }
